137786d5d1f68380c98308cd001aee7568107650
Muhammad Ridwan Syah <91133666+mhmmdrdwnsyh@users.noreply.github.com>
2021-12-02 14:01:41 +0700

    Create LoginLogic.java

---
+++ LoginLogic.java
@@ -0,0 +1,58 @@
+package programm;
+
+import java.util.Scanner;
+
+public class program_asal {
+	public static void main (String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		String password;
+		
+		System.out.println(" "); //this is the space
+		System.out.println("Halo,selamat datang website acara toktok");
+
+		
+		System.out.println("Silahkan masukkan nama anda: ") ; password = input.nextLine();
+
+		//System.out.println(" ");
+		
+		if(password != "Wildan") {
+			System.out.print("Maaf anda tidak terdaftar sebagai peserta acara");
+		} else {
+			System.out.print("Anda terdaftar sebagai peserta acara");
+		}
+		
+	
+		
+	}
+}
+package programm;
+
+import java.util.Scanner;
+
+public class program_asal {
+	public static void main (String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		String password;
+		
+		System.out.println(" "); //this is the space
+		System.out.println("Halo,selamat datang website acara toktok");
+
+		
+		System.out.println("Silahkan masukkan nama anda: ") ; password = input.nextLine();
+
+		//System.out.println(" ");
+		
+		if(password != "Wildan") {
+			System.out.print("Maaf anda tidak terdaftar sebagai peserta acara");
+		} else {
+			System.out.print("Anda terdaftar sebagai peserta acara");
+		}
+		
+	
+		
+	}
+}
